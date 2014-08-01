89e9c4c34ed628cfc04d606623a7e3c3fb7eea70
apseudonym <chrisdavis0205@gmail.com>
2014-07-31 23:17:03 -0500

    Added support for untagged music, reformatted some code

--- musicsort.py
+++ musicsort.py
@@ -1,10 +1,10 @@
-import os
+import os, sys
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 
 musicroot = os.path.join(os.path.expanduser('~'), 'Music')
 
-albums = []
+folders = []
 illegal = r'\/*?:"<>|'
 
 def cleanSort():
@@ -12,33 +12,45 @@ def cleanSort():
 	
 	os.chdir(musicroot)
 
-	getFolders(musicroot)
+	getMusicFolders(musicroot)
 				
-	for i in albums:
+	for i in folders:
 		path = os.path.join(musicroot, i)
 		print(i)
 		songs = findSongs(path)
 		song = songs[0]
-		if '.mp3' in song:
-			songInfo = EasyID3(song)
-		if '.flac' in song:
-			songInfo = FLAC(song)
+		songInfo = getSongInfo(song)
 		try:
 			songArtist = songInfo['artist'][0]
 			songAlbum = songInfo['album'][0]
-			sort(songArtist, songAlbum, i)
 		except KeyError:
-			print('Problem with tags on: {0}, consider retagging and try again'.format(os.path.split(i)[1]))
-			
+			songInfo = tagMusic(i, songs)
+			songArtist = songInfo['artist']
+			songAlbum = songInfo['album']
+		sort(songArtist, songAlbum, i)
+
+	print('Cleaning up...')	
 	emptySweep(musicroot)
 	print('Done sorting!')
+
+def tagMusic(location, songs):
+        print('Problem with tags on "{0}", please manually enter information'.format(os.path.split(location)[1]))
+        artist = input('Artist name? \n')
+        album = input('Album name? \n')
+        for song in songs:
+                songInfo = getSongInfo(song)
+                songInfo['artist'] = artist
+                songInfo['album'] = album
+                songInfo.save()
+        return {'artist': artist, 'album': album}
 		
 def findSongs(location):
 	songs = []
 	for i in os.listdir(location):
-		if '.mp3' in i or '.flac' in i:
+		if isSong(i):
 			songs.append(os.path.join(location, i))
 	return songs
+
 
 def sort(artist, album, directory):
 	for i in illegal:
@@ -53,16 +65,41 @@ def sort(artist, album, directory):
 	try:
 		os.rename(directory, albumlocation)
 	except:
-		#os.mkdir(albumlocation)
 		try:
 			for i in os.listdir(directory):
 				os.renames(os.path.join(directory, i), os.path.join(albumlocation, i))
 		except: 
 			print('\tSome file already exisits, you may want to look at your music')
-		
+
+
+def getMusicFolders(location):
+	directories = []
+	for i in os.listdir(location):
+		if isSong(i):
+			return location
+		if os.path.isdir(os.path.join(location, i)):
+			directories.append(os.path.join(location, i))
+	for directory in directories:
+		check = getMusicFolders(os.path.join(location, directory))
+		if check:
+			folders.append(check)
+
+
+def getSongInfo(song):
+	if '.mp3' in song:
+			songInfo = EasyID3(song)
+	if '.flac' in song:
+			songInfo = FLAC(song)
+	return songInfo
+
+
+def isSong(file):
+	if '.mp3' in file or '.flac' in file:
+		return True
+	return False
+
 
 def emptySweep(location):
-	print('Cleaning up...')
 	items = os.listdir(location)
 	if items == []:
 		return True
@@ -73,21 +110,7 @@ def emptySweep(location):
 			if check:
 				os.removedirs(nextleaf)
 	return False
-        
 
-def getFolders(location):
-	directories = []
-	containsMusic = False
-	for i in os.listdir(location):
-		if '.mp3' in i or '.flac' in i:
-			return location
-		if os.path.isdir(os.path.join(location, i)):
-			directories.append(os.path.join(location, i))
-	for directory in directories:
-		check = getFolders(os.path.join(location, directory))
-		if check:
-			albums.append(check)
-	
 
 if __name__ == '__main__':
 	cleanSort()
